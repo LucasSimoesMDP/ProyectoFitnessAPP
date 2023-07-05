fc0a68c2425db837cbd6aebf7964413387d4574c
LucasSimoesMDP <lucasgsimoes@icloud.com>
2023-07-05 18:40:08 -0300

    Agregado el decorador login_required para que el usuario no pueda acceder a ciertas caracteristicas si no está logueado

--- fitness/views.py
+++ fitness/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
- 
+from django.contrib.auth.decorators import login_required
+from django.db import models
+
+
 # Create your views here.
+
 
 def index(request):
     return render(request, "index.html")
@@ -9,14 +13,25 @@ def index(request):
 def login(request):
       return render(request, "login.html")
 
+def register(request):
+      return render(request, "register.html")
+
+# @login_required
+# def logout(request):
+#       return
+
+@login_required
 def my_profile(request):
     return render(request,"my_profile.html")
 
+@login_required
 def subir_rutina(request):
     return render(request, "subir_rutina1.html")
 
+@login_required
 def subir_rutina_p2(request):
         return render(request, "subir_rutina2.html")
 
+@login_required
 def subir_rutina_p3(request):
         return render(request, "subir_rutina3.html")
