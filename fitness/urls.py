70b1cad6ea5b456149d2ffc5ada79e46641d6223
LucasSimoesMDP <lucasgsimoes@icloud.com>
2023-07-05 18:38:50 -0300

    Si usuario no está logueado, lo lleva al index para que eliga si crearse cuenta o logearse

--- fitness/urls.py
+++ fitness/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import my_profile,index,subir_rutina, subir_rutina_p2, subir_rutina_p3,login
+from .views import register,my_profile,index,subir_rutina, subir_rutina_p2, subir_rutina_p3,login
 from . import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('my_profile/', views.my_profile, name = 'my_profile'),
+    path('accounts/login/', views.index, name='index'),
     path('login/',views.login, name='login'),
+    path('register/',views.register, name='register'),
+    path('my_profile/', views.my_profile, name = 'my_profile'),
     path('subir_rutina1/',views.subir_rutina, name= 'subir_rutina1'),
     path('subir_rutina2/',views.subir_rutina_p2, name= 'subir_rutina2'),
     path('subir_rutina3/',views.subir_rutina_p3, name= 'subir_rutina3'),
